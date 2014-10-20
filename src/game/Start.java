f653397e850c4b743ce0b9d85008d4888c15bab8
Matt Silvey <mtsilvey@gmail.com>
2014-10-20 15:27:48 +0900

    First Push
    Initial push

---
+++ src/game/Start.java
@@ -0,0 +1,17 @@
+package game;
+/**
+ * ZombieRogue Game by Matt Silvey - Cuzit
+ * @author cuzit
+ * Documentation goes here.
+ * This file starts the game.
+ */
+
+class Start {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world.");
+
+	}
+
+}
